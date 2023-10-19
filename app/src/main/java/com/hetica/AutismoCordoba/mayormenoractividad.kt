1d27cc9b8e4076d69771288624e1ce6cb928f137
alvarohghg <lvaro.berjillos@gmail.com>
2023-10-19 17:17:27 +0200

    Feature: gráfica con dias con menor actividad
    Añadida gráfica con los 4 dias de menor actividad dado el mes y el año.

--- app/src/main/java/com/hetica/AutismoCordoba/mayormenoractividad.kt
+++ app/src/main/java/com/hetica/AutismoCordoba/mayormenoractividad.kt
@@ -26,7 +26,6 @@ class mayormenoractividad : AppCompatActivity() {
     private val binding get() = _binding!!
 
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityMayormenoractividadBinding.inflate(layoutInflater)
@@ -35,9 +34,12 @@ class mayormenoractividad : AppCompatActivity() {
         binding.apply {
             diasmayoractividad.animation.duration = animationDuration
             diasmayoractividad.animate(horizontalBarSet)
+
+            diasmenoractividad.animation.duration = animationDuration
+            diasmenoractividad.animate(horizontalBarSet)
         }
 
-        // Supongamos que tienes un EditText con el id "editTextYear"
+
 
         val editTextYear = findViewById<EditText>(R.id.editTextYear)
 
@@ -55,7 +57,7 @@ class mayormenoractividad : AppCompatActivity() {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         spinner.adapter = adapter
 
-// Obtener el índice del mes actual en la lista de meses
+        // Obtener el índice del mes actual en la lista de meses
         val index = meses.indexOf(mesActualEnFormatoString)
         if (index != -1) {
             spinner.setSelection(index) // Establecer la posición predeterminada del Spinner al índice del mes actual
@@ -93,10 +95,19 @@ class mayormenoractividad : AppCompatActivity() {
             override fun afterTextChanged(s: Editable?) {
                 val anyo = s.toString()
                 if (anyo.isNotEmpty()) {
-                    val selectedItem = spinner.selectedItem.toString()
-                    val db = AdminSQLiteOpenHelperStats(this@mayormenoractividad, "Stats.db", null, 1)
-                    obtenerDiasConMinutosEnUnMes(db, selectedItem, anyo)
+                    val yearValue = anyo.toInt()
+                    if (yearValue < 2023) {
+                        Toast.makeText(this@mayormenoractividad, "El año debe ser igual o superior a 2023", Toast.LENGTH_SHORT).show()
+                        return
+                    }else{
+                        val selectedItem = spinner.selectedItem.toString()
+                        val db = AdminSQLiteOpenHelperStats(this@mayormenoractividad, "Stats.db", null, 1)
+                        obtenerDiasConMinutosEnUnMes(db, selectedItem, anyo)
+                    }
+
                 } else {
+                    Toast.makeText(this@mayormenoractividad, "Debe especificar el año de consulta", Toast.LENGTH_SHORT).show()
+
                     // Realizar alguna acción apropiada si el EditText está vacío
                 }
             }
@@ -132,16 +143,30 @@ class mayormenoractividad : AppCompatActivity() {
         if (listaDias.isEmpty()) {
             val mensaje = "No se encontraron resultados para el mes $mes, año $anyo"
             Toast.makeText(this@mayormenoractividad, mensaje, Toast.LENGTH_SHORT).show()
+            binding.diasmayoractividad.animate(emptyList())
+            binding.diasmenoractividad.animate(emptyList())
+
+            return
         }
+
         val listaDiasOrdenados = listaDias.sortedByDescending { it.second } // Ordenar la lista por cantidad de minutos en orden descendente
         val listaDiasTop4 = listaDiasOrdenados.take(4) // Obtener solo los 4 primeros elementos de la lista ordenada
+        val listaDiasTop4Asc = listaDiasOrdenados.takeLast(4) // Obtener los 4 últimos elementos de la lista ordenada
+
+
 
         val listaDiasFloat = listaDiasTop4.map { it.first to it.second.toFloat() }
+        val listaDiasFloatAsc = listaDiasTop4Asc.map { it.first to it.second.toFloat() }
 
         binding.diasmayoractividad.animation.duration = mayormenoractividad.animationDuration
         val data = generateHorizontalBarData(listaDiasFloat)
         binding.diasmayoractividad.animate(data)
         binding.diasmayoractividad.invalidate()
+
+        binding.diasmenoractividad.animation.duration = mayormenoractividad.animationDuration
+        val dataAsc = generateHorizontalBarData(listaDiasFloatAsc)
+        binding.diasmenoractividad.animate(dataAsc)
+        binding.diasmenoractividad.invalidate()
     }
 
 
@@ -149,9 +174,20 @@ class mayormenoractividad : AppCompatActivity() {
 
     private fun generateHorizontalBarData(data: List<Pair<String, Float>>): List<Pair<String, Float>> {
         val mappedData = mutableListOf<Pair<String, Float>>()
-
+        val rectangle2 = findViewById<View>(R.id.rectangle2)
+        val rectangle3 = findViewById<View>(R.id.rectangle3)
         for (i in 0 until data.size) {
             mappedData.add(data[i])
+
+        }
+        if (data.size == 1) {
+            mappedData.add(data[0])
+            rectangle2.visibility = View.VISIBLE
+            rectangle3.visibility = View.VISIBLE
+
+        }else{
+            rectangle2.visibility = View.INVISIBLE
+            rectangle3.visibility = View.INVISIBLE
         }
 
         return mappedData
